587cf5d3feeea383c7c8614014db363e7af82077
Shailesh Chaudhary <shaileshchaudhary@Shaileshs-MacBook-Pro.local>
2017-04-12 01:10:30 -0500

    Transpiling

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        open('http://localhost:' + port);
+    }
+});
